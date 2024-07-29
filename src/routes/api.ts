c0b24c0779c3bc1e8d9685b8e475b424ba320d35
imrany <imranmat254@gmail.com>
2024-07-29 14:16:19 +0300

    updating api routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,11 +2,11 @@ import express from "express"
 import { getUser, deleteUser, getUsers, loginUser, protectUser, registerUser, updateUser } from "../controllers/user"
 const router=express.Router()
 
-router.post("/auth/register",registerUser)
-router.post("/auth/login",loginUser)
-router.get("/accounts/:phone_number",protectUser,getUser)
-// router.get("/accounts",protectUser,getUsers)
-router.patch("/accounts/:phone_number",protectUser,updateUser)
-router.delete("/accounts/:phone_number",protectUser,deleteUser)
+router.post("/auth/sign_up",registerUser)
+router.post("/auth/sign_in",loginUser)
+router.get("/users/:phone_number",protectUser,getUser)
+// router.get("/users",protectUser,getUsers)
+router.patch("/users/:phone_number",protectUser,updateUser)
+router.delete("/users/:phone_number",protectUser,deleteUser)
 
 export default router
